5e37df2d188967b021c05efefc4c2b16e0a102c7
Klim Krivosheev <klim.msu@gmail.com>
2025-06-12 23:45:00 -0400

    Working on Model classes

---
+++ Model/Pace.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Pace
+    {
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+        MeasurmentSystem MeasurmentSystem { get; set; }
+    }
+}
---
+++ Model/RaceResultPrediction.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class RaceResultPrediction
+    {
+        private Pace _pace;
+        private double _distanceInMeters;
+
+        public RaceResultPrediction(Pace pace, double distanceIn_distanceInMetersMeters) 
+        { 
+            _pace = pace;
+            _distanceInMeters = _distanceInMeters;
+
+        }
+        public TimeSpan GetRaceResult()
+        {
+            TimeSpan result = TimeSpan.Zero;
+
+            return result;
+        }
+
+        private int ConvertPaceToSeconds (Pace pace)
+        {
+            int result = 0;
+            result = pace.Minutes * 60 + pace.Seconds;
+            return result;
+        }
+    }
+}
